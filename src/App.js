cb4dc6de67893a1723b1036742485fba159f7f4f
HenriPirinen <henripirinen@hotmail.fi>
2018-07-27 09:37:40 +0300

    Test update

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
+//Test update
+
 import React, { Component } from 'react';
 import './App.css';
 import ToggleButton from './components/toggleButton';
